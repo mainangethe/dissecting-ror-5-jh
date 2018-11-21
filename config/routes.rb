d57c2c2d68619e1f060fa1199b6089e8536e854f
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-11-21 14:34:48 +0300

    configured custom routes for about & contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  get 'about-us', to: 'pages#about'
+  get 'hire-us', to: 'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
