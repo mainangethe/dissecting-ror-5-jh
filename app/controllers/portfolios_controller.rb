5b432a4b8ace03e23ce18bfa96842aa80da3fe05
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-11-21 13:25:09 +0300

    Added show functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
 
   def update
   	@portfolio_item = Portfolio.find(params[:id])
-  	
+
   	respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body) )
         format.html { redirect_to portfolios_path, notice: 'Your Portoflio #{:title} has been updated.' }  
@@ -38,6 +38,10 @@ class PortfoliosController < ApplicationController
 
     end
 
+  end
+
+  def show
+  	@portfolio_item = Portfolio.find(params[:id])
   end
 
 
