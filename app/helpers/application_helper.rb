75d0d659a021de496f6c79073b7bebc8560f6871
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-05 10:06:09 +0300

    Refactor Session Trackers to Session Source View Helper method
    Initially we added our session trackers for each layout independently.
    This led to duplication of code and breaking the DRY principle.
    
    We then recreated the session source trackers as a view helper method -'session_source_helper' in the application helper module.
    It also uses the content_tag to generate the html tag and assigns it a CSS class - 'session-source-greeting'
    
    Relates to JIRA Ticket: BEDT-122

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+	
 	def login_helper
 		if current_user.is_a?(User)
 		  link_to "Logout", destroy_user_session_path, method: :delete
@@ -6,6 +7,13 @@ module ApplicationHelper
 		  (link_to "Register ", new_user_registration_path) +
 		  "<br>".html_safe +
 		  (link_to "Login", new_user_session_path)
+		end
+	end
+
+	def session_source_helper
+		if session[:source]
+			greeting = "Thank you for visiting us from #{session[:source]}"
+			content_tag(:p, greeting, class: "session-source-greeting")
 		end
 	end
 end
