0cc42c6c64bca0aaacfc1652f70ec8655fbd5634
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-05 20:51:48 +0300

    Refactor @Portfolio_items variable in the Portfolio Controller
    Initially we were calling the @portfolio_items independently for several actions in the Portfolio controller.
    This led to duplication of code and breaking the DRY principle.
    
    Solution was to create a method '.set_portfolio_items' method and ensure that it is run before any other action by calling it in the '.before_action' method
    The actions that need this method include : #edit, #show, #update
    
    Relates to JIRA Ticket: BEDT-131

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,4 @@ class ApplicationController < ActionController::Base
 
 	include DefaultPageContentConcern
 
-
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,8 @@
 class PortfoliosController < ApplicationController
 
+  before_action :set_portfolio_items, only: [:edit, :update, :show, :destroy]
+
+  #call unique layout
   layout "portfolio"
   
 	def index
@@ -34,13 +37,10 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-    #3.times { @portfolio_item.technologies.build }
-
   end
 
   def update
-  	@portfolio_item = Portfolio.find(params[:id])
+  	
 
   	respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -54,11 +54,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-  	@portfolio_item = Portfolio.find(params[:id])
   end
 
-  def destroy
-  	@portfolio_item = Portfolio.find(params[:id])
+  def destroy 	
   	@portfolio_item.destroy
   	respond_to do |format|
   		format.html { redirect_to portfolios_path, notice: 'Your portfolio has been removed'}
@@ -73,6 +71,10 @@ class PortfoliosController < ApplicationController
                                           :body, 
                                           technologies_attributes: [:name] 
                                         )
+  end
+
+  def set_portfolio_items
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 
