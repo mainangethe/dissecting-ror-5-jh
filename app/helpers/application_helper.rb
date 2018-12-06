14da33eabc8009c1afd4dad0c422c3bb402598ca
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-06 20:05:37 +0300

    Refactor Footer and Nav to different partials
    We changed the footer to be it's own partial '_application_footer.html.erb' and also fixed the navigation styles including the login helper links.
    
    Also made the home button link to the About us page 'All about Mradi'.
    
    Related to JIRA Ticket: BEDT-150

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 	
-	def login_helper
+	def login_helper(style)
 		if current_user.is_a?(GuestUser)
-		  (link_to "Register ", new_user_registration_path, class: 'nav-link') +
-		  "<br>".html_safe +
-		  (link_to "Login", new_user_session_path, class: 'nav-link')
+		  (link_to "Register ", new_user_registration_path, class: style ) +
+		  " ".html_safe +
+		  (link_to "Login", new_user_session_path, class: style)
 		else
-		  link_to "Logout", destroy_user_session_path, class: 'nav-link', method: :delete
+		  link_to "Logout", destroy_user_session_path, class: style, method: :delete
 		end
 	end
 
