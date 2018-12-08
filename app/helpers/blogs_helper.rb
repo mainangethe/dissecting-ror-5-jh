acf776c69fe590f8f03fc14402c776dddc5403ea
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-08 20:29:46 +0300

    Update Styles for New, Edit, Show Blog posts and Refactor Sidebar
    We updated the sidebar to be it's own partial.
    
    Also we updated the new post, edit post and show post views with BS4 styles.
    
    Relates to JIRA Ticket: BEDT-160

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,8 +2,8 @@ module BlogsHelper
 	def new_blog_link_helper
 		if ( logged_in?(:site_admin) )
   			
-  			link = (link_to 'Write a New Post', new_blog_path)
-  			msg = "Want to share a thought? | " + link
+  			link = (link_to 'Write a New Post', new_blog_path, class: 'btn btn-success')
+  			msg = "Want to share a thought? " + link
   			
   			content_tag(:h3, msg.html_safe, class: "pb-3 mb-4 font-italic border-bottom")	
   			
