fa6e2264141cd4650f45096e22a4704c602f258b
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-10 17:11:04 +0300

    Refactor the navigation to a single helper method & Add active indicator for application layout
    We migrated all the link generation for each navigation to be within 1 helper method to help reduce code duplication.
    The helper method also makes use of an array with the navigation and path stored as hashes to allow flexibility and scaling in future.
    
    We also added another boolen method that returns active to help add the active class to the navigation of the application layout to show an active page.
    For the Portfolio and Blog layout though , this is not effective as both layouts don't make use of an active status to show current page.
    
    Relates to JIRA Tickets: BEDT-167, BEDT-168

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,46 @@ module ApplicationHelper
 	def copyright_generator_helper
 		MradiViewTool::Renderer.copyright('Mradi Kenya Limited', 'All rights reserved')
 	end
+
+	def navigation_array
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},
+			
+			{
+				url: about_us_path,
+				title: 'About Us'
+			},
+			
+			{
+				url: hire_us_path,
+				title: 'Contacts'
+			},
+			
+			{
+				url: blogs_path,
+				title: 'Blog'
+			},
+			
+			{
+				url: portfolios_path,
+				title: 'Portfolio'
+			}
+		]
+	end
+
+	def navigation_generator_helper(css_style, html_tag)
+		nav_links = ''
+		navigation_array.each do |link|
+			nav_links << "<#{html_tag}> <a href='#{ link[:url] }' class='#{css_style} #{active?(link[:url])}'> #{link[:title]} </a> </#{html_tag}>"
+		end
+		
+		nav_links.html_safe
+	end
+
+	def active?(path)
+		"active" if current_page?(path)
+	end
 end
