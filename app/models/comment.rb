4c182ce8673291f17c1c8a9a0d53fae65f1f2439
Ng'ethe Maina <ngethe@mradi.co.ke>
2018-12-28 17:25:48 +0300

    Add broadcast job for Comment feature
    Created a comment broadcast job
    Configured it's websocket route
    Updated the comment model with a callback
    
    Relates to JIRA Ticket: BEDT-196

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :blog
+	belongs_to :user
+	belongs_to :blog
 
-  validates :content,
+	validates :content,
   			presence: true,
   			length: { minimum: 10, maximum: 1000 }
+
+  	after_create_commit { CommentBroadcastJob.perform_later(self) }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
 
-
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
