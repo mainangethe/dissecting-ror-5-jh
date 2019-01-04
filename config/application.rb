d0a4bef2ca554eebd8a4ff3d460190f66c904e4b
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-01-04 22:01:48 +0300

    Update Call to Secret Key Base
    Put a unique secret key base for Devise in the application.rb
    
    Relates to JIRA Ticket: BEDT-217

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module MradiPortfolio
     config.generators.system_tests = nil
 
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
